b1f60264692fe8a01b42c5db446e4780b1f7521c
gabyRiela <gabrielasc.3dmodel@gmail.com>
2025-09-21 15:19:10 -0700

    Lab1 homework
    Week1 Lab Homework

---
+++ lab1/tempConversion.js
@@ -0,0 +1,8 @@
+let fahrenheit = 75
+//the average temperature in Mexico city on May is between 60 - 70F
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console. log(fahrenheit, "temperatureFahrenheits")
+console. log("temperatureCelsius", celsius)
+console. log("temperatureKelvin", kelvin)
